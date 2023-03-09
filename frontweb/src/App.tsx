489911b27725116e4634bbfe6904408e72426f82
anadeso <anadeso90@gmail.com>
2023-03-09 08:10:52 -0300

    Customizando tema bootstrap

--- frontweb/src/App.tsx
+++ frontweb/src/App.tsx
@@ -1,10 +1,9 @@
-import React from 'react';
-import logo from './logo.svg';
+import './assets/styles/custom.scss';
 import './App.css';
 
 function App() {
   return (
-   <h1>Olá DSCatalog</h1>
+   <h1 className="bg-primary text-white">Olá DSCatalog</h1>
   );
 }
 
