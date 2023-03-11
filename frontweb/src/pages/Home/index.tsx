f534513d94c1d02d0f56127c5bf938e8efc3a6ce
anadeso <anadeso90@gmail.com>
2023-03-11 18:11:26 -0300

    Iniciando estilizacao

--- frontweb/src/pages/Home/index.tsx
+++ frontweb/src/pages/Home/index.tsx
@@ -1,19 +1,23 @@
 import { ReactComponent as MainImage } from 'assets/images/main-image.svg';
 import NavBar from "components/NavBar";
 
+import './styles.css';
+
 const Home = () => {
     return (
       <>
         <NavBar/>
         <div className="home-container">
+
           <div className="home-card">
+
             <div className="home-content-container">
               <h1>Conheça o melhor catálogo de produtos</h1>
             </div>
             <div className="home-image-container">
               <MainImage />
             </div>
-
+            
           </div>
 
         </div>
