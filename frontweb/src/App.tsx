27f4c8abde052423cb01f6f3f663eccd3994885b
anadeso <anadeso90@gmail.com>
2023-03-11 17:13:27 -0300

    Pagina home estrutura basica html

--- frontweb/src/App.tsx
+++ frontweb/src/App.tsx
@@ -1,13 +1,10 @@
+import Home from 'pages/Home';
 import './assets/styles/custom.scss';
-import NavBar from 'components/NavBar';
 import './App.css';
 
 const App = () => {
   return (
-    <>
-      <NavBar/>
-      <></>
-    </>
+    <Home />
   )
 }
 
