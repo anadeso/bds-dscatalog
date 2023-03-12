dce53c1b75ce8241656ae17656e60533da0ae7c7
anadeso <anadeso90@gmail.com>
2023-03-12 20:58:57 -0300

    ButtonIcon customizado

--- frontweb/src/components/ButtonIcon/index.tsx
+++ frontweb/src/components/ButtonIcon/index.tsx
@@ -1,10 +1,17 @@
-import './styles.css'
+import './styles.css';
+
+import { ReactComponent as Icon } from 'assets/images/seta.svg';
 
 const ButtonIcon = () => {
   return (
-    <button className="btn btn-primary button-icon">
-      <h6>INICIE AGORA A SUA BUSCA</h6>
-    </button>
+    <div className="btn-container">
+      <button className="btn btn-primary">
+        <h6>INICIE AGORA A SUA BUSCA</h6>
+      </button>
+      <div className="btn-icon-container">
+        <Icon />
+      </div>
+    </div>
   );
 };
 
