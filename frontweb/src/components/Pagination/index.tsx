75c28be6505caeee4fb46fceb7f163ba54456653
anadeso <anadeso90@gmail.com>
2023-03-15 07:50:20 -0300

    Pagination estilizacao

--- frontweb/src/components/Pagination/index.tsx
+++ frontweb/src/components/Pagination/index.tsx
@@ -1,16 +1,18 @@
 import { ReactComponent as ArroyIcon } from 'assets/images/seta.svg';
 
+import './styles.css';
+
 const Pagination = () => {
 
     return (
         <div className="pagination-container"> 
-            <ArroyIcon />
-            <div className="pagination-item" >1</div>
+            <ArroyIcon className="arrow-previous arrow-inactive"/>
+            <div className="pagination-item active" >1</div>
             <div className="pagination-item" >2</div>
             <div className="pagination-item" >3</div>
             <div className="pagination-item" >...</div>
             <div className="pagination-item" >10</div>
-            <ArroyIcon />
+            <ArroyIcon className="arrow-next arrow-native" />
         </div>
     );
 }
