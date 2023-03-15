774aba72dc7e1c5b8d097d2309858d92b7171870
anadeso <anadeso90@gmail.com>
2023-03-15 08:32:39 -0300

    Admin layout

--- frontweb/src/pages/Admin/index.tsx
+++ frontweb/src/pages/Admin/index.tsx
@@ -1,11 +1,14 @@
-import Navbar from "./Navbar";
+import Navbar from './Navbar';
+
+import './styles.css';
 
 const Admin = () => {
   return (
     <div className="admin-container">
       <Navbar />
-      <div className="admin-content"></div>
-      <h1>Conteudo</h1>
+      <div className="admin-content">
+        <h1>Conteudo</h1>
+      </div>
     </div>
   );
 };
