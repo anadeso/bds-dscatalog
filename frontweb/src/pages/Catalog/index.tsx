e60f73f55f99701c77afd86bdd478146e61b790d
anadeso <anadeso90@gmail.com>
2023-03-22 08:58:22 -0300

    Incluindo Loaders na pagina

--- frontweb/src/pages/Catalog/index.tsx
+++ frontweb/src/pages/Catalog/index.tsx
@@ -7,11 +7,13 @@ import { SpringPage } from 'types/vendor/spring';
 import { AxiosParams } from 'types/vendor/axios';
 import { useEffect, useState } from 'react';
 import { BASE_URL } from 'util/request';
+import CardLoader from './CardLoader';
 
 import './styles.css';
 
 const Catalog = () => {
   const [page, setPage] = useState<SpringPage<Product>>();
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     const params: AxiosParams = {
@@ -23,8 +25,12 @@ const Catalog = () => {
       },
     };
 
+    setIsLoading(true);
     axios(params).then((response) => {
       setPage(response.data);
+    })
+    .finally(() => {
+      setIsLoading(false);
     });
   }, []);
 
@@ -33,15 +39,19 @@ const Catalog = () => {
       <div className="row catalog-title-container">
         <h1>Catálogo de produtos</h1>
       </div>
+
+
       <div className="row">
-        {page?.content.map((product) => (
+        {isLoading ? <CardLoader /> : (
+          page?.content.map((product) => (
           <div className="col-sm-6 col-lg-4 col-xl-3" key={product.id}>
             <Link to="/products/1">
               <ProductCard product={product} />
             </Link>
           </div>
-        ))}
+        )))}
       </div>
+
       <div className="row">
         <Pagination />
       </div>
