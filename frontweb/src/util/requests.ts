2c3541f98aeb7da46f9d2ce6f187c3647762c22f
anadeso <anadeso90@gmail.com>
2023-05-18 08:09:11 -0300

    Decodificando o token JWT

--- frontweb/src/util/requests.ts
+++ frontweb/src/util/requests.ts
@@ -1,6 +1,15 @@
 import axios, { AxiosRequestConfig } from 'axios';
 import qs from 'qs';
 import history from './history';
+import jwtDecode from 'jwt-decode';
+
+type Role = 'ROLE_OPERATOR' | 'ROLE_ADMIN';
+
+type TokenData = {
+    exp: number;
+    user_name: string;
+    authorities: Role[];
+}
 
 type LoginResponse = {
     access_token: string,
@@ -92,3 +101,12 @@ axios.interceptors.response.use(function (response) {
     return Promise.reject(error);
 });
 
+export const getTokenData = () : TokenData | undefined => {
+
+    try {
+        // queremos um retorno do tipo TokenData
+        return jwtDecode(getAuthData().access_token) as TokenData;
+    } catch(error) {
+        return undefined;
+    }
+}
