771ea30d241fe5d13367ccc3d2a028444fec803d
anadeso <anadeso90@gmail.com>
2023-05-25 08:29:33 -0300

    Permissionamento em nivel de rotas

--- frontweb/src/pages/Admin/index.tsx
+++ frontweb/src/pages/Admin/index.tsx
@@ -17,7 +17,7 @@ const Admin = () => {
           <PrivateRoute path="/admin/categories">
             <h1>Catefory CRUD</h1>
           </PrivateRoute>
-          <PrivateRoute path="/admin/users">
+          <PrivateRoute path="/admin/users" roles={['ROLE_ADMIN']}>
             <Users />
           </PrivateRoute>
         </Switch>
